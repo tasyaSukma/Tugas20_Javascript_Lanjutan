fb9ed114dbeeae314a3b6a16783e5d9d95ca6e6b
tasyaSukma <maftuhah.tasya@gmail.com>
2020-09-21 10:54:06 +0700

    Add files via upload

---
+++ tugas20.js
@@ -0,0 +1,4 @@
+var txt = "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable."
+
+console.log(txt.match("are"))
+console.log(txt.match("words"))
